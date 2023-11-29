6d1c31838b817a511588afeb673a9c4a08e2ddca
rckarthik3001 <hellokarthik736@gmail.com>
2023-11-29 14:33:43 +0530

    Updated weather details

--- weather/index.js
+++ weather/index.js
@@ -5,7 +5,8 @@ let cityName = document.getElementById("cityname");
 let btn = document.getElementById("submit");
 apikey = "ae73831443fa1f8daf2fed4cf629df2a";
 let img = document.getElementById("tempImage");
-
+let humidity = document.getElementById("hum");
+let wind = document.getElementById("windspeed");
 function convert(val){
     return (val - 273).toFixed(2);
 }
@@ -19,11 +20,14 @@ btn.addEventListener('click',function()
     .then(data =>{
         var temp = data['main']['temp'];
         var descr = data['weather'][0]['description'];
+        var h = data['main']['humidity'];
+        var ws = data['wind']['speed'];
 
         cityName.innerHTML = `Weather of ${input.value}`;
        tempValue.innerHTML = `${convert(temp)} &#8451;`;
        description.innerHTML = descr;   
-       
+       humidity.innerHTML = `Humidity: <span>${h}%</span>`;
+       wind.innerHTML = `Wind Speed: <span>${ws} m/s</span>`;
        if(descr=="sunny"){
        img.src = 'weather/sunny.png';
        }
